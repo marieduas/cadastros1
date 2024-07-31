e9f72efd73e5494ee6fb4a559bbb65a6b745c8c4
cadastros <malvesdasi8@gmail.com>
2024-07-31 11:30:47 -0300

    003

--- cadastros/MainPage.xaml.cs
+++ cadastros/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace cadastros;
+﻿using Microsoft.Maui.Controls;
+
+namespace cadastros;
 
 public partial class cadastros : ContentPage
 {
@@ -9,6 +11,11 @@ public partial class cadastros : ContentPage
 		InitializeComponent();
 	}
 
+	     // Optionally, you can add event handlers for button clicks here
+        // For example:
+        // private void OnClienteButtonClicked(object sender, EventArgs e)
+        // {
+        //     // Handle the button click event
+        // }
 	
 }
-
