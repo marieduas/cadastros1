5a3c46737d05a5085137d7a11318f16b3adfeb4a
cadastros <malvesdasi8@gmail.com>
2024-08-16 11:04:28 -0300

    16/08

--- cadastros/Cliente.xaml.cs
+++ cadastros/Cliente.xaml.cs
@@ -7,5 +7,13 @@ namespace cadastros
             InitializeComponent();
           
         }
+        public void botaovoltar(object sender, EventArgs args)
+        {
+                Application.Current.MainPage = new MainPage();
+        }
+        public void botaosalvar(object sender, EventArgs args)
+        {
+                Application.Current.MainPage = new MainPage();
+        }
     }
-}
+    }
